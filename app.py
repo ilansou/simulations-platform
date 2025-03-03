cfbf66b10d438fc77bfee76d6a4842acdf4d9a34
ilansou <ilanshushan13@gmail.com>
2025-03-03 19:41:31 +0200

    wsl

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import streamlit as st
 # Define the pages
 dashboard = st.Page("routes/dashboard.py", title="Dashboard", icon=":material/dashboard:")
 experiment_details = st.Page("routes/experiment_details.py", title="Experiment Details", icon=":material/description:")
+query_page = st.Page("routes/query.py", title="Query Simulation Data", icon=":material/search:")
 
 # Set up navigation
-pg = st.navigation([dashboard, experiment_details])
+pg = st.navigation([dashboard, experiment_details, query_page])
 
 # Set page configuration
 st.set_page_config(page_title="Simulation Manager", page_icon=":material/science:", layout="wide")
